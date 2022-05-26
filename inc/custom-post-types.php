fcdb17234c3e4e773d0b1c6bcc19c2fe0f4991c9
Amir Ali <amirliaqat2020@gmail.com>
2022-05-26 11:41:05 +0500

    Adding new custom post type for about us section.

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -104,4 +104,30 @@ function bkd_adding_team_section() {
 }
 add_action( 'init', 'bkd_adding_team_section' );
 
+/************** Function Adding custom post type for about section **************/
+function bkd_adding_about_section() {
+    $args = array(
+        'label' => 'About Us',
+        'description' => '',
+        'menu_icon' => 'dashicons-format-gallery',
+        'menu_position' => 5,
+        'public' => true,
+        'hierarchical' => false,
+        'exclude_from_search' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'show_in_rest' => true,
+		'can_export' => true,
+		'has_archive' => true,
+        'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+        'taxonomies' => array(),
+		'capability_type' => 'page',
+    );
+
+    register_post_type( 'about', $args );
+}
+add_action( 'init', 'bkd_adding_about_section' );
+
 ?>
