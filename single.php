4bac7f19020e81291a7b9193c62243ff99837145
Amir Ali <amirliaqat2020@gmail.com>
2022-05-26 16:32:16 +0500

    Creating single post file.

--- single.php
+++ single.php
@@ -4,4 +4,18 @@
  *
  * @package Byte Ki Duniya
 */
+get_header();
 ?>
+
+    <!-- Simple Page-->
+    <section class="page-section mt-5" id="about">
+        <div class="container">
+            <div class="text-center mt-5">
+                <?php the_post_thumbnail( 'large' ); ?>
+                <h2 class="section-heading text-uppercase mt-5"><?php the_title(); ?></h2>
+            </div><!--text-center-->
+            <p><?php the_content(); ?></p>
+        </div><!--container-->
+    </section>
+
+<?php get_footer(); ?>
