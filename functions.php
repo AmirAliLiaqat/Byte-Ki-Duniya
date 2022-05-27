8d501f71b61a60ce6b7ea09e2b69ca01a0aa11ac
Amir Ali <amirliaqat2020@gmail.com>
2022-05-27 11:02:18 +0500

    Converting font awesome styles to scripts.

--- functions.php
+++ functions.php
@@ -30,11 +30,11 @@ add_action( 'after_setup_theme', 'bkd_theme_setup' );
 /************** Function using for adding styles and scripts links **************/
 function bkd_enqueue_scripts() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
-    wp_enqueue_style( 'Font-Awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js' );
     wp_enqueue_style( 'Google-Fonts-1', 'https://fonts.googleapis.com/css?family=Montserrat:400,700' );
     wp_enqueue_style( 'Google-Fonts-2', 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' );
-
+    
     wp_enqueue_script( 'script', THEME_DIR_URL . '/assets/js/script.js' );
+    wp_enqueue_script( 'Font-Awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js' );
     wp_enqueue_script( 'jQuery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js' );
     wp_enqueue_script( 'Popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js' );
     wp_enqueue_script( 'Bootstrap-min', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js' );
